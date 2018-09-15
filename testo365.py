82b8dc6c17f3211a856b8e7fa716c011d7c85f42
ytkwmr <yuta519.ka23tf@docomo.ne.jp>
2018-09-15 19:39:02 +0900

    FirstCommit

---
+++ testo365.py
@@ -0,0 +1,16 @@
+import urllib
+# f = urllib.urlopen("https://endpoints.office.com/version?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7")
+f = urllib.urlopen('https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7')
+res = f.read()
+print(res) # <html>...
+
+
+# import requests
+
+# response = requests.get('https://endpoints.office.com/endpoints/worldwide')
+
+# print(response.text)
+
+# 'https://endpoints.office.com/version'
+# 'https://endpoints.office.com/endpoints/worldwide'
+# 'https://endpoints.office.com/changes/worldwide/0000000000'
