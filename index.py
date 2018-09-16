ada5448fc63c80e2d5b6e628c3e06c665ee19688
ytkwmr <yuta519.ka23tf@docomo.ne.jp>
2018-09-16 18:29:47 +0900

    modify index.py

--- index.py
+++ index.py
@@ -1,9 +1,17 @@
 #coding:utf-8
-import os
-from bottle import route, run
+import urllib
+# f = urllib.urlopen("https://endpoints.office.com/version?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7")
+f = urllib.urlopen('https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7')
+res = f.read()
+print(res) # <html>...
 
-@route("/")
-def hello_world():
-        return "hello world"
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT",5000)))
+# import requests
+
+# response = requests.get('https://endpoints.office.com/endpoints/worldwide')
+
+# print(response.text)
+
+# 'https://endpoints.office.com/version'
+# 'https://endpoints.office.com/endpoints/worldwide'
+# 'https://endpoints.office.com/changes/worldwide/0000000000'
